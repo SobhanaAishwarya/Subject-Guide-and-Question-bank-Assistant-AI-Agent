02e6359511ca3e518f878e32bbf15311bb577ae3
SobhanaAishwarya <aishwaryasobhana@gmail.com>
2026-07-20 20:12:40 +0530

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import runpy
-
-APP_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "studyai_streamlit", "studyai")
-sys.path.insert(0, APP_DIR)
-os.chdir(APP_DIR)
-
-runpy.run_path(os.path.join(APP_DIR, "streamlit_app.py"), run_name="__main__")
